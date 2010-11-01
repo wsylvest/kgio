250d798f2e6b5a6efc8c0e125a7c0d578ed7f92f
William Sylvester <wsylvest@gmail.com>
2010-11-01 14:10:24 -0400

    fixed OSX problem

--- ext/kgio/missing/ancient_ruby.h
+++ ext/kgio/missing/ancient_ruby.h
@@ -1,13 +1,14 @@
 #ifndef MISSING_ANCIENT_RUBY_H
 #define MISSING_ANCIENT_RUBY_H
 
+/* 
 #ifndef HAVE_RB_STR_SET_LEN
 static void rb_str_set_len(VALUE str, long len)
 {
 	RSTRING(str)->len = len;
 	RSTRING(str)->ptr[len] = '\0';
 }
-#endif /* ! HAVE_RB_STR_SET_LEN */
+#endif */ /* ! HAVE_RB_STR_SET_LEN */
 
 #ifndef RSTRING_PTR
 #  define RSTRING_PTR(s) (RSTRING(s)->ptr)
